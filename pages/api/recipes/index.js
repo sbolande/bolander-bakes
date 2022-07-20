7c1867c69b9b0c20f78fc362fb56f17b9bd84bab
Seth Bolander <bol15015@byui.edu>
2022-07-20 12:05:30 -0600

    updated FETCH to not use callbacks

--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -1,46 +1,36 @@
 import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
-  // connect to mongo
-  MongoClient.connect(process.env.MONGO_URL)
-    .then((client) => {
-      console.log("Successfully connected to Mongo.");
+  // fetch query params
+  const category = req.query.category;
+  const favorites = req.query.favorites;
+  const name = req.query.name;
+  const ingredients = req.query.ingredients;
+  const instructions = req.query.instructions;
+  let query = {};
+  if (category) query.category = category;
+  if (favorites) query.favorite = true;
+  if (name) query.name = { $regex: name };
+  if (ingredients) query.ingredients = { $regex: ingredients };
+  if (instructions) query.instructions = { $regex: instructions };
 
-      // fetch query params
-      const category = req.query.category;
-      const favorites = req.query.favorites;
-      const name = req.query.name;
-      const ingredients = req.query.ingredients;
-      const instructions = req.query.instructions;
-      let query = {};
-      if (category) query.category = category;
-      if (favorites) query.favorite = true;
-      if (name) query.name = { $regex: name };
-      if (ingredients) query.ingredients = { $regex: ingredients };
-      if (instructions) query.instructions = { $regex: instructions };
+  try {
+    // connect to mongo
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    // query collection
+    const db = client.db(process.env.MONGO_DB);
+    const collection = db.collection("recipes");
+    const recipes = await collection.find(query).toArray();
+    client.close();
 
-      // query collection
-      const db = client.db(process.env.MONGO_DB);
-      db.collection("recipes")
-        .find(query)
-        .toArray()
-        .then((recipes) => {
-          if (recipes === null || recipes.length === 0) {
-            console.error("No documents found in db.");
-          }
-
-          res.status(200).json(recipes);
-        })
-        .finally(() => {
-          // close client regardless
-          client.close();
-        });
-
-      return;
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send({ message: "Internal server error." });
-      return;
-    });
+    if (recipes === null || recipes.length === 0) {
+      console.log("No documents found in db.");
+    }
+    res.status(200).json(recipes);
+    return;
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal server error." });
+    return;
+  }
 }
